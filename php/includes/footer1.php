8a0fd520162f61fc7270ecca9138dd2b78f182c0
zerme <zerme.29@gmail.com>
2020-11-26 16:19:28 -0600

    footer chido

---
+++ php/includes/footer1.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="/css/footer1.css">
+<div class="mt-5 pt-5 pb-5 footer">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-5 col-xs-12 about-company">
+      <h2>TrueNews</h2>
+      <p class="pr-5 text-white-50">TrueNews es una empresa que se encargara de que tus noticias sean de lo mas confiables y seguras, basta de noticias fake.</p>
+      <p><a href="#"><i class="fa fa-facebook-square mr-1"></i></a><a href="#"><i class="fa fa-linkedin-square"></i></a></p>
+    </div>
+    <div class="col-lg-3 col-xs-12 links">
+      <h4 class="mt-lg-0 mt-sm-3">Categorias</h4>
+        <ul class="m-0 p-0">
+          <li><a href="#">Criminal</a></li>
+          <li><a href="#">Politica</a></li>
+          <li><a href="#">Deportes</a></li>
+          <li><a href="#">Ciencia</a></li>
+          <li><a href="#">Tecnologia</a></li>
+          <li><a href="#">Otros</a></li>
+        </ul>
+    </div>
+    <div class="col-lg-4 col-xs-12 location">
+      <h4 class="mt-lg-0 mt-sm-4">Location</h4>
+      <p>Universidad Autonoma de Aguascalientes</p>
+      <p class="mb-0"><i class="fa fa-phone mr-3"></i>(541) 754-3010</p>
+      <p><i class="fa fa-envelope-o mr-3"></i>TrueNews@gmail.com</p>
+    </div>
+  </div>
+  <div class="row mt-5">
+    <div class="col copyright">
+      <p class=""><small class="text-white-50">© 2019. All Rights Reserved.</small></p>
+    </div>
+  </div>
+</div>
+</div>
