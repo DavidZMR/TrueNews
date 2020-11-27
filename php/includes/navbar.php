c5d53c9c75b87b9c5e17ccc859bb34ebfaa7c134
zerme <zerme.29@gmail.com>
2020-11-27 10:44:08 -0600

    jalando

--- php/includes/navbar.php
+++ php/includes/navbar.php
@@ -16,7 +16,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/php/index.php">Inicio <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Acerca de Nosotros</a>
