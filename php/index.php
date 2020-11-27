0e7f67c91c898c5aff990add1068b7e402cd8a4f
zerme <zerme.29@gmail.com>
2020-11-26 18:00:38 -0600

    inicio terminado

--- php/index.php
+++ php/index.php
@@ -4,14 +4,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/css/index.css">
 </head>
 <body>
     <?php include("includes/navbar.php"); ?>
+    <div class="container">
+        <div class="row-full" style="background: #343a40;">
+            <div class="row justify-content-around" >
+                <div class="col-2 categoria text-center"> 
+                    <a href="#"><img class="cat" src="/resourses/img/ciminal.jpg" alt=""></a><br>
+                    <p>Criminal</p>
+                </div>
+                <div class="col-2 categoria text-center"> 
+                    <a href="#"><img class="cat" src="/resourses/img/politica.jpg" alt=""></a><br>
+                    <p>Politica</p>
+                </div>
+                <div class="col-2 categoria text-center"> 
+                    <a href="#"><img class="cat" src="/resourses/img/deportes.jpg" alt=""></a><br>
+                    <p>Deportes</p>
+                </div>
+                <div class="col-2 categoria text-center"> 
+                    <a href="#"><img class="cat" src="/resourses/img/ciencia.jpg" alt=""></a><br>
+                    <p>Ciencia</p>
+                </div>
+                <div class="col-2 categoria text-center"> 
+                    <a href="#"><img class="cat" src="/resourses/img/tecnologia.jpg" alt=""></a><br>
+                    <p>Tecnologia</p>
+                </div>
+                <div class="col-2 categoria text-center"> 
+                    <a href="#"><img class="cat" src="/resourses/img/otros.png" alt=""></a><br>
+                    <p>Otros</p>
+                </div>
+            
+            </div>
+        </div>
+        <div class="row-full">
+            <div class="row">
+                <div class="col-9" id="noticias">
+
+                </div>
+                <div class="col-3 ">
+
+                </div>
+            </div>
+        </div>
+    </div>
     
 
-    <h1>hijos de puta</h1>
-    <p></p>
-
-    <?php include("includes/footer.php"); ?>
+    <?php include("includes/footer1.php"); ?>
 </body>
 </html>
